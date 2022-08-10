313773e9eed5beb8213d2f71ef1cdec0030d0b86
ohalex <alex.oh.han@gmail.com>
2022-08-10 19:15:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -69,7 +69,8 @@ cat <<EOF >"$BUILD_SCRIPT"
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS $FF_CONFIGURE \
         --extra-cflags='$FF_CFLAGS' --extra-cxxflags='$FF_CXXFLAGS' \
         --extra-ldflags='$FF_LDFLAGS' --extra-ldexeflags='$FF_LDEXEFLAGS' --extra-libs='$FF_LIBS' \
-        --extra-version="\$(date +%Y%m%d)"
+        --extra-version="\$(date +%Y%m%d)"  --disable-ffprobe --disable-ffplay --disable-doc --disable-avdevice \
+        --disable-coreimage --enable-libx264 --disable-appkit --disable-avfoundation
     make -j\$(nproc) V=1
     make install install-doc
 EOF
